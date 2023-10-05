d32bb1f0078da4a4aa23b4f8994e7255caa2a0a7
Stefano Pacella <47741309+StefanPacella@users.noreply.github.com>
2023-10-05 10:07:49 +0200

    Update UserDeserializer.java

--- src/main/java/it/project/chat/rest/jackson/UserDeserializer.java
+++ src/main/java/it/project/chat/rest/jackson/UserDeserializer.java
@@ -40,7 +40,7 @@ public class UserDeserializer extends JsonDeserializer<User> {
 		if (node.has("picture")) {
 			r.setPicture(node.get("picture").asText());
 		} else {
-			r.setPicture("PictureUtility.DEFAULTPICTUREPROFILE");
+			r.setPicture(PictureUtility.DEFAULTPICTUREPROFILE);
 		}
 
 		if (node.has("version")) {
